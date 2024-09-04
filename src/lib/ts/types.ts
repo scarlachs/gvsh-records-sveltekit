ab23dcfb5ffe858b15edfd86e70429a0de62529c
Pascal <butler.ergot_0s@icloud.com>
2024-09-04 19:29:34 +0200

    add types

---
+++ src/lib/ts/types.ts
@@ -0,0 +1,34 @@
+import type { Picture } from "vite-imagetools";
+
+type stack =
+	| "html"
+	| "css"
+	| "tailwindcss"
+	| "shadcn"
+	| "shadcn-svelte"
+	| "bootstrap"
+	| "scss"
+	| "javascript"
+	| "typescript"
+	| "sveltekit"
+	| "webpack"
+	| "astro"
+	| "react"
+	| "nextjs"
+	| "typo3"
+	| "storyblok";
+
+type ProjectText = {
+	title: string;
+	copy: string;
+	website: string;
+	github?: string;
+	usedStack: stack[];
+};
+
+type ProjectImage = {
+	file: Picture;
+	alt?: string;
+};
+
+export type Project = { text: ProjectText; image: ProjectImage };
