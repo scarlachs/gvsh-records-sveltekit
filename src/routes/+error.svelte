a8a377efa8fa2ed3a289dfb136a8f74687da0b50
Pascal <butler.ergot_0s@icloud.com>
2024-09-05 00:59:48 +0200

    add if for other errors than 404

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	import Button from "$lib/components/ui/button/button.svelte";
 	import { page } from "$app/stores";
+	import { error } from "@sveltejs/kit";
 </script>
 
 <section class="container flex min-h-96 items-center">
 	<div class="pt-16">
-		<h1 class="mb-5 text-3xl font-bold lg:text-5xl">{$page.status}: Seite nicht gefunden</h1>
-		<p class="mb-10 text-lg">Leider existiert die von dir gesuchte Seite nicht.</p>
-		<Button href="/" variant="secondary">Zur Startseite</Button>
+		<h1 class="mb-5 text-3xl font-bold lg:text-5xl">
+			{$page.status}: {$page.status === 404 ? "Seite nicht gefunden" : $page.error?.message}
+		</h1>
+		{#if $page.status === 404}
+			<p class="mb-10 text-lg">Leider existiert die von dir gesuchte Seite nicht.</p>
+			<Button href="/" variant="secondary">Zur Startseite</Button>
+		{/if}
 	</div>
 </section>
