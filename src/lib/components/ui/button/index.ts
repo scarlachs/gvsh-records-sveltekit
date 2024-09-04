46e8f8a7bbb24cbe32e897cf4d86263ce857df42
Pascal <butler.ergot_0s@icloud.com>
2024-09-04 19:29:27 +0200

    fix bbutton styling to match websites theme

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -3,15 +3,13 @@ import { type VariantProps, tv } from "tailwind-variants";
 import Root from "./button.svelte";
 
 const buttonVariants = tv({
-	base: "focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium motion-safe:transition-colors focus-visible:outline-none focus-visible:ring-1 disabled:pointer-events-none disabled:opacity-50",
+	base: "inline-flex items-center justify-center min-w-min rounded-md text-sm font-medium motion-safe:transition-colors focus-visible:outline-none focus-visible:ring-2 disabled:pointer-events-none disabled:opacity-50 focus-visible:ring-foreground",
 	variants: {
 		variant: {
-			default: "bg-primary text-primary-foreground hover:bg-primary/90 shadow",
-			destructive:
-				"bg-destructive text-destructive-foreground hover:bg-destructive/90 shadow-sm",
+			default: "bg-primary text-primary-foreground hover:bg-primary/90",
 			outline:
-				"border-input bg-background hover:bg-accent hover:text-accent-foreground border shadow-sm",
-			secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80 shadow-sm",
+				"border-input bg-background hover:bg-accent hover:text-accent-foreground border",
+			secondary: "bg-secondary text-secondary-foreground hover:bg-secondary/80",
 			ghost: "hover:bg-accent hover:text-accent-foreground",
 			link: "text-primary underline-offset-4 hover:underline"
 		},
@@ -19,7 +17,7 @@ const buttonVariants = tv({
 			default: "h-9 px-4 py-2",
 			sm: "h-8 rounded-md px-3 text-xs",
 			lg: "h-10 rounded-md px-8",
-			icon: "h-9 w-9"
+			icon: "size-9"
 		}
 	},
 	defaultVariants: {
