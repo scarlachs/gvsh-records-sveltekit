2bec9f6b12ab04a3ba58d5fe976a78fc18a7da32
Pascal <butler.ergot_0s@icloud.com>
2024-09-07 12:14:07 +0200

    add new pages

---
+++ src/routes/(prose)/+layout.svelte
@@ -0,0 +1,7 @@
+<section class="container">
+	<div
+		class="prose prose-slate mb-16 pt-16 dark:prose-invert prose-headings:hyphens-auto prose-headings:[word-break:break-word] prose-a:hyphens-none prose-a:[word-break:break-word] md:mb-24 [&_*[id]]:scroll-mt-20"
+	>
+		<slot />
+	</div>
+</section>
---
+++ src/routes/(prose)/impressum/+page.svelte
@@ -0,0 +1,46 @@
+<script>
+	const title = "Impressum | Pascal Schaar";
+</script>
+
+<svelte:head>
+	<meta name="robots" content="noindex, nofollow" />
+	<meta name="format-detection" content="telephone=no" />
+
+	<meta name="title" content={title} />
+	<title>{title}</title>
+
+	<meta property="og:title" content={title} />
+</svelte:head>
+
+<h1>Impressum</h1>
+
+<p>
+	Pascal Schaar<br />
+	Bornwiese 2<br />
+	21391 Reppenstedt
+</p>
+
+<h2>Kontakt</h2>
+<p>
+	{@html "E-Mail: in<!-- @wn -->fo<!-- .qmd- -->@sc<!-- $d;@ -->haa<!-- .com -->r.dev"}
+</p>
+
+<h2>Umsatzsteuer-ID</h2>
+<p>
+	Umsatzsteuer-Identifikationsnummer gemäß § 27 a Umsatzsteuergesetz:<br />
+	DE315467726
+</p>
+
+<h2>Verbraucherstreitbeilegung/Universalschlichtungsstelle</h2>
+<p>
+	Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer
+	Verbraucherschlichtungsstelle teilzunehmen.
+</p>
+
+<p>
+	Quelle: <a
+		href="https://www.e-recht24.de/impressum-generator.html"
+		target="_blank"
+		rel="noopener">https://www.e-recht24.de/impressum-generator.html</a
+	>
+</p>
