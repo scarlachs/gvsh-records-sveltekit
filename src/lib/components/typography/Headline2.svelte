bd5386aeec0e877957527ffcf4c3f4cc76627230
Pascal <butler.ergot_0s@icloud.com>
2025-01-25 19:20:16 +0100

    update props

--- src/lib/components/typography/Headline2.svelte
+++ src/lib/components/typography/Headline2.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
 	import { cn } from "$lib/ts/utils";
+	import type { ClassProps, ChildrenProps } from "$lib/ts/types";
 
-	type Props = {
-		class?: string | undefined | null;
-		children?: import("svelte").Snippet;
-	};
-
-	let { class: className = undefined, children }: Props = $props();
+	let { class: className = undefined, children }: ClassProps & ChildrenProps = $props();
 </script>
 
 <h1 class={cn("text-2xl font-bold md:text-3xl", className)}>
