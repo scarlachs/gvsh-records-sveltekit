75abee4080890477ab05e29155476f9fcf353793
Pascal Schaar <butler.ergot_0s@icloud.com>
2025-07-13 22:40:29 +0200

    fix typo

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -6,6 +6,6 @@ export const load = (async () => {
 	return {
 		title: "GVSH Rekorde Kraftdreikampf",
 		description:
-			"Hier findest du eine Liste aller Kraftdreikampf-Rekore im Gewichtheber-Verband Schleswig-Holstein von 1982 e.V."
+			"Hier findest du eine Liste aller Kraftdreikampf-Rekorde im Gewichtheber-Verband Schleswig-Holstein von 1982 e.V."
 	};
 }) satisfies PageServerLoad;
