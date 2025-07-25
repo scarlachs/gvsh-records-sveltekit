ecbf0bc0e6aeb6108ebbe97e1ecd7f8949bb83dd
Pascal Schaar <butler.ergot_0s@icloud.com>
2025-07-25 21:50:18 +0200

    remove logo

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import PascalLogo from "$lib/components/logos/PascalLogo.svg.svelte";
 	import ModeToggle from "$lib/components/ModeToggle.svelte";
 	import SocialNav from "$lib/components/SocialNav.svelte";
 	import Separator from "$lib/components/ui/separator/separator.svelte";
@@ -10,14 +9,6 @@
 	class="supports-backdrop-filter:bg-background/60 sticky top-0 z-50 overflow-clip border-b border-slate-900/10 backdrop-blur dark:border-slate-50/10"
 >
 	<div class="container flex items-center justify-end py-2">
-		<!-- <a
-			class="focus-visible:ring-foreground focus-visible:ring-offset-background h-11 rounded-md focus-visible:ring-2 focus-visible:ring-offset-4 focus-visible:outline-none"
-			href="/"
-		>
-			<PascalLogo class="inline-flex h-full w-auto" />
-			<span class="sr-only">Pascal</span>
-		</a> -->
-
 		<div class="flex h-9 items-center space-x-2">
 			<nav class="flex items-center" aria-label="Hauptnavigation">
 				{#each ["Männer", "Frauen"] as item}
