91da4b136e3dfee0e4bf377104746a658fe2aca2
Pascal Schaar <butler.ergot_0s@icloud.com>
2025-07-25 21:51:13 +0200

    update links

--- src/lib/ts/constants.ts
+++ src/lib/ts/constants.ts
@@ -1,8 +1,6 @@
 const links = {
 	linkedin: "https://www.linkedin.com/in/pascalschaar/",
-	calcom: "https://cal.com/pascalschaar/coffee",
-	github: "https://github.com/scarlachs",
-	whatsapp: "https://wa.me/message/BY4II5TIXXW6N1"
+	github: "https://github.com/scarlachs"
 };
 
 export { links };
